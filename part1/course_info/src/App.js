a416abd072e184fd98e072c55e425b78d6ceb349
Kade <kp5431@g.rit.edu>
2021-05-22 12:05:11 -0500

    Exercise 1.5 complete

--- part1/course_info/src/App.js
+++ part1/course_info/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
     {
       name: 'Fundamentals of React',
       exercises: 10
@@ -16,11 +17,12 @@ const App = () => {
       exercises: 14
     }
   ]
+}
   return (
     <>
-      <Header course={course} />
-      <Content parts = {parts}/>
-      <Total parts = {parts}/>
+      <Header course={course.name} />
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
     </>
   )
 }
