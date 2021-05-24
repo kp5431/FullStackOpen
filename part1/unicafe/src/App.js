c719c4a81a554b2bb846d706693a73f9cf50228b
Kade <kp5431@g.rit.edu>
2021-05-23 22:32:16 -0500

    Exercise 1.6 complete

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header text = "give feedback"/>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Header text = "statistics"/>
+      <Feedback text = "good" num = {good}/>
+      <Feedback text = "neutral" num = {neutral}/>
+      <Feedback text = "bad" num = {bad}/>
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  //console.log('props value is', props)
+  return (
+    <button onClick = {handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Header = ({text}) => {
+  return(
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Feedback = ({text, num}) => {
+  return(
+    <div>
+      {text} {num}
+    </div>
+  )
+}
+
+
+export default App
