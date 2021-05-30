f87584b0cc76a1aa83ed4b086dc192b55c0b9552
Kade <kp5431@g.rit.edu>
2021-05-30 18:28:13 -0500

    Exercise 1.13 complete

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -9,15 +9,26 @@ const App = () => {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-   
-  const [selected, setSelected] = useState(0)
-  const getRandomInt = () => Math.floor(Math.random() * anecdotes.length)
-  const handleAnecdote = () => setSelected(getRandomInt())
+
+  const [selected, setSelected] = useState(0) //sets selected up as a state variable initialized to 0
+  const getRandomInt = () => Math.floor(Math.random() * anecdotes.length) //returns a random integer within range 0 to length of anecdotes - 1
+  const handleAnecdote = () => setSelected(getRandomInt()) //function describes what to do when anecdote button is clicked. It sets selected to the random num
+
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0)) //sets votes up as a state variable initialized to an array of length anecdotes full of 0's
+  const handleVote = () => { //describes what to do when votes button is clicked. 
+    const copy = [...votes] //create an identical copy of votes
+    copy[selected] += 1 //increase val by 1 at selected index
+    setVotes(copy) //sets votes to the new copy array. Can't mutate a datastructure when it's part of a React component's state.
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
       <p>
+        {anecdotes[selected]} <br></br>
+        has {votes[selected]} votes
+      </p>
+      <p>
+        <Button handleClick = {handleVote} text = "vote"/>
         <Button handleClick = {handleAnecdote} text = "next anecdote"/>
       </p>
     </div>
