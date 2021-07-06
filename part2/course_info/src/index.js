02d3ee3848534eb400d499e699e11487fa503385
Kade <kp5431@g.rit.edu>
2021-07-06 16:19:04 -0500

    Exercise 2.1 complete

---
+++ part2/course_info/src/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+/*
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
+}
+
+const Part = ({name, exercises}) => {
+  return (
+    <li>
+      {name} {exercises}
+    </li>    
+  )
+}
+*/
+const Content = ({ course }) => {
+  const lparts = course.parts
+  return (
+    <div>
+      <ul>
+        {lparts.map(part =>
+          <li key={part.id}> 
+            {part.name} {part.exercises}
+          </li>
+          )}
+      </ul>
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content course = {course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
