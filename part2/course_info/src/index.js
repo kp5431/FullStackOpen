8ff93ee7eedba0bf78b6588e011f00a1c2010a8a
Kade <kp5431@g.rit.edu>
2021-07-11 17:53:49 -0400

    Exercise 2.4 complete

--- part2/course_info/src/index.js
+++ part2/course_info/src/index.js
@@ -20,7 +20,7 @@ const Total = ({array}) => {
     return accum + currVal.exercises
   }, 0)
   return(
-    <h3>Number of exercises {sum}</h3>
+    <h3>total of {sum} exercises</h3>
   ) 
 }
 /*
@@ -57,34 +57,66 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
-
-  return <Course course={course} />
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+/*
+{parts.map(part =>
+          <li key={part.id}> 
+            {part.name} {part.exercises}
+          </li>
+          )}
+*/
+  return (
+    <div>
+      <ul>
+        {courses.map(course =>
+         <Course key={course.id} course={course} />
+          )}
+      </ul>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
