191252b9c3081136e1edea829e67cbba63640c48
Kade <kp5431@g.rit.edu>
2021-07-11 18:00:31 -0400

    Exercise 2.5 complete

--- part2/course_info/src/index.js
+++ part2/course_info/src/index.js
@@ -1,60 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({name}) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Total = ({array}) => {
-  //how this works:
-  //iter 1: a = 0, c = 10
-  //iter 2: a = 10, c = 7
-  //iter 3: a = 17, c = 14
-  //iter 4: a = 31, c = 11
-  //iter 5: a = 42
-  //accum is an integer every time, but have to hardcode the initial '0' val in as second arg.
-  //then add obj.exercises each iter (currVal.exercises)
-  const sum = array.reduce((accum, currVal) => {
-    return accum + currVal.exercises
-  }, 0)
-  return(
-    <h3>total of {sum} exercises</h3>
-  ) 
-}
-/*
-const Part = ({name, exercises}) => {
-  return (
-    <li>
-      {name} {exercises}
-    </li>    
-  )
-}
-*/
-const Content = ({parts}) => {
-  return (
-    <div>
-      <ul>
-        {parts.map(part =>
-          <li key={part.id}> 
-            {part.name} {part.exercises}
-          </li>
-          )}
-      </ul>
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts} />
-      <Total array={course.parts} />
-    </div>
-  )
-}
+import Course from './course'
 
 const App = () => {
   const courses = [
@@ -101,13 +47,7 @@ const App = () => {
       ]
     }
   ]
-/*
-{parts.map(part =>
-          <li key={part.id}> 
-            {part.name} {part.exercises}
-          </li>
-          )}
-*/
+
   return (
     <div>
       <ul>
