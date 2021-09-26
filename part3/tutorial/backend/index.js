a66e083c3c7e8bef1598ca22bf5df5468ef1d93d
Kade <kp5431@g.rit.edu>
2021-09-25 21:54:32 -0400

    part3 tutorial complete

--- part3/tutorial/backend/index.js
+++ part3/tutorial/backend/index.js
@@ -29,18 +29,18 @@ Middleware functions
 */
 
 const requestLogger = (request, response, next) => { 
-console.log('Method:', request.method)
-console.log('Path:  ', request.path)
-console.log('Body:  ', request.body)
-console.log('---')
-next()
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
 }
 
 const errorHandler = (error, request, response, next) => { 
     console.error(error.message)
   
     if(error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' })
     } 
     else if(error. name === 'ValidationError'){
         return response.status(400).json({error: error.message})
@@ -75,26 +75,27 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id).then(note => {
-        if(note) {
-            response.json(note)
-        }
-        else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+    Note.
+        findById(request.params.id).then(note => {
+            if(note) {
+                response.json(note)
+            }
+            else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error)) //send the error to be handled in the next middleware
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error)) //send the error to be handled in the next middleware
 })
 
-app.put('/api/notes/:id', (request, response) => {
+app.put('/api/notes/:id', (request, response, next) => {
     const body = request.body
 
     const note = {
@@ -103,31 +104,31 @@ app.put('/api/notes/:id', (request, response) => {
     }
 
     Note.findByIdAndUpdate(request.params.id, note, {new: true})
-    .then(updatedNote => {
-        response.json(updatedNote)
-    })
-    .catch(error => next(error))
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+        return response.status(400).json({ error: 'content missing' })
     }
 
     const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
     })
 
     note.save()
-    .then(savedNote => {
-        return savedNote.toJSON()
-    })
-    .then(formattedNote => response.json(formattedNote))
-    .catch(err => next(err)) //pass to errorhandler
+        .then(savedNote => {
+            return savedNote.toJSON()
+        })
+        .then(formattedNote => response.json(formattedNote))
+        .catch(err => next(err)) //pass to errorhandler
 })
 
 app.use(unknownEndpoint)
