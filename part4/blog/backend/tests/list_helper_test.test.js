543283bf4daba0dc1dfc35f488f58e79b60812f5
Kade <kp5431@g.rit.edu>
2021-09-30 15:36:01 -0400

    Exercise 4.5 complete

--- part4/blog/backend/tests/list_helper_test.test.js
+++ part4/blog/backend/tests/list_helper_test.test.js
@@ -112,6 +112,163 @@ describe('Total Likes', () => {
       test('likes field is a string not a number', () => {
         expect(() => listHelper.totalLikes(blogLikesWrongType)).toThrow('likes field must be number')
       })
-      
+})
 
+describe('Max Likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('When list has only one blog, max is the only blog in there', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual(listWithOneBlog[0])
+  })
+
+  const listWithManyBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 6,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 4,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 3,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 2,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 1,
+      __v: 0
+    },
+  ]
+
+  test('test of 6 different blogs', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual(listWithManyBlogs[0])
+  })
+
+  const blogMissingLikesField = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      __v: 0
+    }
+  ]
+
+  test('blog missing likes field', () => {
+    expect(() => listHelper.favoriteBlog(blogMissingLikesField)).toThrow('No likes field in blog')
+  })
+
+  const blogLikesWrongType = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: '5',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      __v: 0
+    }
+  ]
+
+  test('likes field is a string not a number', () => {
+    expect(() => listHelper.favoriteBlog(blogLikesWrongType)).toThrow('likes field must be number')
+  })
+
+  const listWithManyBlogs0Likes = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 0,
+      __v: 0
+    },
+  ]
+
+  test('test of 6 different blogs with same num of likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs0Likes)
+    expect(result).toEqual(listWithManyBlogs0Likes[0])
+  })
 })
--- part4/blog/backend/utils/list_helper.js
+++ part4/blog/backend/utils/list_helper.js
@@ -6,9 +6,9 @@ const dummy = (blogs) => 1
 
 /**
  * Returns the total sum of likes in all of the blog posts
- * @param {*} posts An array of blog posts 
+ * @param {*} blogs An array of blog posts 
  */
- const totalLikes = (posts) => {
+ const totalLikes = (blogs) => {
     const sumLikes = (currSum, nextBlog) => {
         if(!(nextBlog.hasOwnProperty('likes'))){
             throw new ReferenceError('No likes field in blog')
@@ -19,11 +19,35 @@ const dummy = (blogs) => 1
         return currSum + nextBlog.likes
 
     }
-    return posts.reduce(sumLikes, 0)
+    return blogs.reduce(sumLikes, 0)
 }
+
+/**
+ * Figures out which blog has the most likes from a list of blogs.
+ * If more than one blog has the highest number of likes, then return
+ * only 1 of them
+ */
+ const favoriteBlog = (blogs) => {
+    const max = (last, nextBlog) => {
+        return last >= nextBlog.likes ? last : nextBlog.likes
+    }
+
+    const likes = blogs.map((blog) => {
+        if(!(blog.hasOwnProperty('likes'))){
+            throw new ReferenceError('No likes field in blog')
+        }
+        if(!(typeof blog.likes === 'number')){
+            throw new TypeError('likes field must be number')
+        }
+        return blog.likes
+    })
+    const maxLikes = blogs.reduce(max, 0)
+    return blogs[likes.indexOf(maxLikes)]
+ }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
 
